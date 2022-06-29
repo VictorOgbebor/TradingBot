db99c7ea77655252f3eaf4cba8e77f32572fe283
VictorOgbebor <victor.ogbebor@outlook.com>
2022-06-29 02:30:02 -0500

    firstCommit...clean..

--- js/contracts/Arbitrager.sol
+++ js/contracts/Arbitrager.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
-// https://medium.com/coinmonks/flashloan-with-aave-v3-9f94868aeff4
 import "./AaveFlashloanSimpleContract.sol";
 
 // create withdraw function
+
 contract Arbitrager {
   AaveFlashloanSimpleContract aave;
 
