80f95e7160d80fd9fda803f20d39efc38061c14c
VictorOgbebor <victor.ogbebor@outlook.com>
2022-06-29 04:14:02 -0500

    final git push for today 6-29-22

--- js/contracts/Arbitrager.sol
+++ js/contracts/Arbitrager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Victor Ogbebor
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./defi-interfaces/aave/interfaces/IPool.sol";
